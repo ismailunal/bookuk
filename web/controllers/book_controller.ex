119c5cd7901e7e362444c7562eeb44b1d4b1f368
ismailunal <ismailunal@gmail.com>
2017-04-12 22:38:33 +0100

    book controller create

--- web/controllers/book_controller.ex
+++ web/controllers/book_controller.ex
@@ -3,13 +3,23 @@ defmodule Bookuk.BookController do
     
     alias Bookuk.Book
 
-    def new(conn, params) do
+    def new(conn, _params) do
         changeset = Book.changeset(%Book{}, %{})
 
         render conn, "new.html", changeset: changeset
     end
 
-    def create(conn, params) do
-        
+    def create(conn, %{"book" => book}) do
+        changeset = Book.changeset(%Book{}, book)
+
+        case Repo.insert(changeset) do
+            {:ok, _book} ->
+                conn
+                |> put_flash(:info, "Book Created")
+                |> redirect(to: book_path(conn, :index))
+            {:error, changeset} ->
+                render conn, "new.html", changeset: changeset
+        end
+     
     end
 end
